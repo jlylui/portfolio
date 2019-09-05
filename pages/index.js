66f696c94d8fc29bd8623886ac294037e9888f7e
Lui <Joycelyn.Lui.2@team.telstra.com>
2019-09-05 17:43:48 +1000

    initial commit - setup next.js framework

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Index = () => {
+  return (
+    <div>
+      <Head>
+        <title>Joycelyn Lui</title>
+      </Head>
+      <p>Hi!</p>
+    </div>
+  );
+};
+
+export default Index;
