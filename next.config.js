6c6a0d7684daf36ce22d4170a841b4600a255cf9
Joycelyn Lui <joycelyn.lui@hotmail.com>
2019-09-30 18:56:29 +1000

    removed unused variable from next.config.js

--- next.config.js
+++ next.config.js
@@ -5,36 +5,33 @@ const Dotenv = require("dotenv-webpack");
 const withCSS = require("@zeit/next-css");
 const optimizedImages = require("next-optimized-images");
 
-module.exports = withCSS(
-{
-    optimizeImagesInDev: true,
-    webpack: function(config) {
-      config.module.rules.push({
-        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 100000,
-            name: "[name].[ext]"
-          }
+module.exports = withCSS({
+  webpack: function(config) {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000,
+          name: "[name].[ext]"
         }
-      });
-      config.plugins = config.plugins || [];
-      config.plugins = [
-        ...config.plugins,
-        new Dotenv({
-          path: path.join(__dirname, ".env"),
-          systemvars: true
-        })
-      ];
-      return config;
-    },
-    webpackDevMiddleware: config => {
-      config.watchOptions = {
-        poll: 1000, // Check for changes every second
-        aggregateTimeout: 300 // delay before rebuilding
-      };
-      return config;
-    }
+      }
+    });
+    config.plugins = config.plugins || [];
+    config.plugins = [
+      ...config.plugins,
+      new Dotenv({
+        path: path.join(__dirname, ".env"),
+        systemvars: true
+      })
+    ];
+    return config;
+  },
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000, // Check for changes every second
+      aggregateTimeout: 300 // delay before rebuilding
+    };
+    return config;
   }
-);
+});
